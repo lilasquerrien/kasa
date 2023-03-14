a6d4942b6ab02ea7a6b357818b267cb245fc2e5f
Lilas Querrien <lilasquerrien@hotmail.fr>
2023-03-14 15:07:08 +0100

    14/03

--- src/components/Rental/Slideshow.jsx
+++ src/components/Rental/Slideshow.jsx
@@ -1,36 +1,51 @@
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import RentalsListing from '../../datas/RentalsListing.json';
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
 const Slideshow = () => {
-    const { id } = useParams();
+
+    const { id } = useParams()
+    const [currentPictureIndex, setCurrentPictureIndex] = useState(0)
     let rental = RentalsListing.find(i => i.id === id);
-     
-    // Nb d'images à afficher
-    const picturesNb = Math.min(rental.pictures.length);
-     
-    // Tableau pour chaque image
-    const pictures = [];
-    for (let i = 0; i < picturesNb; i++) {
-        pictures.push(
-        <div key={i} className="each-slide-effect">
-            <div style={{ 'backgroundImage': `url(${rental.pictures[i]})` }}></div>
-        </div>
-      );
-    }
-     
+
+    const leftSlide = () => {
+        if (currentPictureIndex === 0) {
+          setCurrentPictureIndex(rental.pictures.length - 1);
+        } else {
+          setCurrentPictureIndex(currentPictureIndex - 1);
+        }
+      };
+      
+      const rightSlide = () => {
+        if (currentPictureIndex === rental.pictures.length - 1) {
+          setCurrentPictureIndex(0);
+        } else {
+          setCurrentPictureIndex(currentPictureIndex + 1);
+        }
+      };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentPictureIndex(currentPictureIndex === rental.pictures.length - 1 ? 0 : currentPictureIndex + 1)
+        }, 3500)
+        return () => clearInterval(interval)
+    }, [currentPictureIndex, rental.pictures.length])
+
     return (
-        <section id='slideshow'>
-            <Slide>
-                {pictures}
-            </Slide>
-            <div className="position">
-                <p>{pictures.number}/{pictures.length}</p>
+        <div className="slideshow"
+            style={{ backgroundImage: `url(${rental.pictures[currentPictureIndex]})`}}>
+            <div className={rental.pictures.length === 1 ? "slidehow__icon--hidden" : "slideshow__icon"}>
+                <button className="slideshow__btn" onClick={leftSlide}><FontAwesomeIcon icon={faChevronLeft} style={{color:"#FFFFFF"}} alt="Left Chevron"/></button>
+                <button className="slideshow__btn" onClick={rightSlide}><FontAwesomeIcon icon={faChevronRight} style={{color:"#FFFFFF"}} alt="Right Chevron"/></button>
             </div>
-        </section>
-    );
-};
+            { rental.pictures.length > 1 &&
+            <p className="slideshow__text">{currentPictureIndex +1}/{rental.pictures.length}</p>
+          }
+        </div>
+    )
+}
 
 export default Slideshow;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,11 +7,15 @@ import Mountains from '../assets/Mountains.png';
 
 function About () {
     return (
-        <section className='aboutContentContainer'>
+        <>
+        
             <Header />
+
+            <section className='aboutContentContainer'>
             <Banner 
             image={ Mountains }
             />
+            <section className='aboutCollapsible'>
             <Collapsible
                 label={'Fiabilité'}
                 children={'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'}
@@ -28,8 +32,13 @@ function About () {
                 label={'Sécurité'}
                 children={'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'}
             />
+            </section>
+            </section>
+
             <Footer />
-        </section>
+            
+        </>
+     
     );
 }
 
